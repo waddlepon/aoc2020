5688f06be513956164d04621e39a60bd49e83ca6
Tyler Poon <tylerpoon@uchicago.edu>
2020-12-15 03:59:46 -0800

    Day14

--- day14/day14a.hs
+++ day14/day14a.hs
@@ -1,5 +1,44 @@
 module Day14A where
 
+import qualified Data.IntMap as IntMap
+import Control.Monad.State
+import Data.Char
+import Data.List
+import Numeric (showHex, showIntAtBase)
+
+type Memory = IntMap.IntMap Int
+type Mask = String
+
+intToBinary :: Int -> String
+intToBinary i = replicate (36 - length out) '0' ++ out
+    where
+        out = showIntAtBase 2 intToDigit i ""
+
+binaryToInt :: String -> Int
+binaryToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+addValue :: (Int, Int) -> State Mask Memory
+addValue (i, v) = do
+    mask <- get
+    let bv = intToBinary v
+    pure $ IntMap.singleton i (binaryToInt $ zipWith (\x y -> if x == 'X' then y else x) mask bv)
+
+changeMask :: Mask -> State Mask Memory
+changeMask mask = do 
+    put mask
+    pure IntMap.empty
+
+stringToInstruction :: String -> State Mask Memory
+stringToInstruction s
+    | inst == "mask" = changeMask $ words s !! 2
+    | otherwise = addValue (read $ takeWhile isDigit $ drop 4 s, read $ words s !! 2)
+    where
+        inst = take 4 s
+
 solve :: String -> IO ()
 solve file = do
     x <- readFile file
+    let ls = map stringToInstruction $ lines x
+    let endMemory = foldl' (flip IntMap.union) IntMap.empty $ evalState (sequence ls) ""
+    print $ endMemory
+    print $ sum $ IntMap.elems endMemory
--- day14/day14b.hs
+++ day14/day14b.hs
@@ -1,5 +1,56 @@
 module Day14B where
 
+import qualified Data.IntMap as IntMap
+import Control.Monad.State
+import Data.Char
+import Data.List
+import Data.Maybe
+import Numeric (showHex, showIntAtBase)
+
+type Memory = IntMap.IntMap Int
+type Mask = String
+
+intToBinary :: Int -> String
+intToBinary i = replicate (36 - length out) '0' ++ out
+    where
+        out = showIntAtBase 2 intToDigit i ""
+
+binaryToInt :: String -> Int
+binaryToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+posAddr :: String -> [String]
+posAddr s
+    | 'X' `elem` s = (posAddr $ before ++ ['0'] ++ after) ++ (posAddr $ before ++ ['1'] ++ after)
+    | otherwise = [s]
+    where
+        i = fromJust $ elemIndex 'X' s
+        (before, _:after) = splitAt i s
+
+addValue :: (Int, Int) -> State Mask Memory
+addValue (i, v) = do
+    mask <- get
+    let bi = intToBinary i 
+    let maskon x y = if x == '0' then y else x
+    let maskedbi = zipWith maskon mask bi
+    let addr = map binaryToInt $ posAddr maskedbi
+    pure . IntMap.unions $ map (`IntMap.singleton` v) addr
+
+changeMask :: Mask -> State Mask Memory
+changeMask mask = do 
+    put mask
+    pure IntMap.empty
+
+stringToInstruction :: String -> State Mask Memory
+stringToInstruction s
+    | inst == "mask" = changeMask $ words s !! 2
+    | otherwise = addValue (read $ takeWhile isDigit $ drop 4 s, read $ words s !! 2)
+    where
+        inst = take 4 s
+
 solve :: String -> IO ()
 solve file = do
     x <- readFile file
+    let ls = map stringToInstruction $ lines x
+    let endMemory = foldl' (flip IntMap.union) IntMap.empty $ evalState (sequence ls) ""
+    print $ sum $ IntMap.elems endMemory
+
